7f41d53adfb894c1b181810317ef76a0152a5ff8
KutsunaSubaRya <index20010928@gmail.com>
2026-01-01 21:20:00 +0800

    refactor!: convert to pure frontend static site
    Remove all backend functionality (server/, shared/, database configs) and dependencies to enable static site deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import HomePage from "@/pages/HomePage";
@@ -17,12 +15,11 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
+    <TooltipProvider>
         <Toaster />
         <Router />
       </TooltipProvider>
-    </QueryClientProvider>
+    
   );
 }
 
